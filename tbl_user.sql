1adb265a04121e46af2d56b9a7f2dcc58df91393
zamanboks <xzamanboks@gmail.com>
2020-04-06 08:07:05 +0600

    sql table new update

--- tbl_user.sql
+++ tbl_user.sql
@@ -1,8 +1,6 @@
---0K--
 -- phpMyAdmin SQL Dump
 -- version 4.8.2
 -- https://www.phpmyadmin.net/
---
 -- Host: 127.0.0.1
 -- Generation Time: Apr 05, 2020 at 01:36 PM
 -- Server version: 10.1.34-MariaDB
